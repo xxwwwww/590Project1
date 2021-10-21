79aab5a7d6d9ae4a2afbd3777ce8ea9e4cc9e892
zezhong98 <>
2021-10-21 17:50:26 -0400

    quick sort completed

--- project1.py
+++ project1.py
@@ -3,16 +3,18 @@ Math 560
 Project 1
 Fall 2021
 
-Partner 1:
-Partner 2:
-Date:
+Partner 1: Zezhong Zhang
+Partner 2: Yitong Wang
+Date: 10/21/2021
 """
+
+from math import floor
 
 """
 SelectionSort
 
-@listToSort : original list to be sorted
-@return : sorted list
+@:listToSort original list to be sorted
+@:return sorted list
 
 Time complexity :　O(n2) for all cases
 """
@@ -57,11 +59,53 @@ QuickSort
 
 Sort a list with the call QuickSort(listToSort),
 or additionally specify i and j.
+
+@:listToSort the unsorted list to be sorted
+@:i the left bound of this sorting step(inclusive)
+@:j the right bound of this sorting step(exclusive)
+@:return the sorted list
 """
 def QuickSort(listToSort, i=0, j=None):
     # Set default value for j if None.
     if j == None:
         j = len(listToSort)
+
+    # if only one element left, return it
+    if j <= (i+1):
+        return listToSort
+
+    # find a pivot which is the middle number in array
+    m = floor((i+j)/2)
+
+    # partition array, put all numbers smaller to left and all numbers bigger to right
+    # set left pointer as l
+    l = i
+    # set right pointer as r
+    r = j-1
+    while l<r:
+        # stop at pivot or an element bigger than pivot
+        while (l < m) and (listToSort[l] < listToSort[m]):
+            l+=1
+        # stop at pivot or an element smaller than pivot
+        while (r > m) and (listToSort[r] >= listToSort[m]):
+            r-=1
+        # swap the two elements
+        if r > l :
+            # swap the two
+            temp = listToSort[r]
+            listToSort[r] = listToSort[l]
+            listToSort[l] = temp
+            # if m is one of them, reset m
+            if m==l:
+                m = r
+            elif m == r:
+                m = l
+
+    # recursively sort left part and right part
+    QuickSort(listToSort, i, m)
+    QuickSort(listToSort, m+1, j)
+
+    # return sorted list
     return listToSort
 
 """
